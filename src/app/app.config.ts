20974af352362cf75721fd316f1d6492b9497aa3
selahattin <selahattindindas@outlook.com>
2024-05-16 22:35:05 +0300

    feat: create custom httpclientservice

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,18 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    {
+      provide: 'baseUrl', 
+      useValue: environment.apiUrl, 
+      multi: true
+    }
+  ]
 };
